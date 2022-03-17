d4124fa2e00603b3eb45a9841b59d43b5a928a18
jmishra <jmishra@evertz.com>
2022-03-17 17:04:40 +0530

    Deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
 	reactStrictMode: false,
 	images: {
 		domains: ["avatars.githubusercontent.com"],
-	}
+	},
+	exportPathMap: function () {
+		return {
+		  "/": { page: "/" },
+		}
+	},
+	assetPrefix: !debug ? 'https://jstjyoti.github.io/' : '',
 };
